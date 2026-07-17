5a844199dabec71d5cf9024c75ba224740e55d75
yyyyy <u5yuugoo@gmail.com>
2026-07-18 08:55:39 +0900

    fix: migrationsを.に

--- database/migration.go
+++ database/migration.go
@@ -17,7 +17,7 @@ func migrateTables(db *sql.DB) error {
 	if err := goose.SetDialect("mysql"); err != nil {
 		return fmt.Errorf("failed to set goose dialect: %w", err)
 	}
-	if err := goose.Up(db, "migrations"); err != nil {
+	if err := goose.Up(db, "."); err != nil {
 		return fmt.Errorf("failed to run migrations: %w", err)
 	}
 	return nil
